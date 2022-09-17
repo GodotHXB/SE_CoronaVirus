17674a7c34a7480697177f52fd95c31c990bda20
GodotHXB <371502435@qq.com>
2022-09-17 16:26:17 +0800

    merge excel (turn to function)

--- merge_excel.py
+++ merge_excel.py
@@ -4,21 +4,18 @@ import pandas as pd
 
 dfs = []
 
-lst = os.scandir("D:/CoronaVirus/data/special_area_excels") # 疫情通报数据本地位置
-for file in lst:
-    name = file.name
-    print(name)
-    df = pd.read_excel("D:/CoronaVirus/data/special_area_excels/" + name)
-    dfs.append(df)
-result = pd.concat(dfs)
-result.to_excel("港澳台数据总表.xlsx",index=False)
+def merging(path,filename):
+    dfs.clear()
+    lst = os.scandir(path) # 疫情通报数据本地位置
+    for file in lst:
+        name = file.name
+        df = pd.read_excel(path + name)
+        dfs.append(df)
+    result = pd.concat(dfs)
+    result.to_excel(filename,index=False)
 
-# lst = os.scandir("D:/CoronaVirus/data/province_excels") # 疫情通报数据本地位置
-# for file in lst:
-#     name = file.name
-#     if(name.endswith("新增感染者数据.xlsx")):
-#         print(name)
-#         df = pd.read_excel("D:/CoronaVirus/data/province_excels/" + name)
-#         dfs.append(df)
-# result = pd.concat(dfs)
-# result.to_excel("各省份新增感染者数据总表.xlsx",index=False)
+if __name__ == '__main__':
+    #merging("D:/CoronaVirus/data/special_area_excels/", "港澳台数据总表.xlsx")
+    #merging("D:/CoronaVirus/data/province_excels/", "各省份新增确诊数据总表.xlsx")
+    #merging("D:/CoronaVirus/data/province_excels/", "各省份新增无症状感染者数据总表.xlsx")
+    merging("D:/CoronaVirus/data/main_land_excels/", "中国大陆本土数据总表.xlsx")
