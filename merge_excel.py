4786880abeb17b1ec444321e49d2e96eb3d6fce7
GodotHXB <371502435@qq.com>
2022-09-17 20:39:09 +0800

    final version

--- merge_excel.py
+++ merge_excel.py
@@ -15,7 +15,7 @@ def merging(path,filename):
     result.to_excel(filename,index=False)
 
 if __name__ == '__main__':
-    #merging("D:/CoronaVirus/data/special_area_excels/", "港澳台数据总表.xlsx")
-    #merging("D:/CoronaVirus/data/province_excels/", "各省份新增确诊数据总表.xlsx")
-    #merging("D:/CoronaVirus/data/province_excels/", "各省份新增无症状感染者数据总表.xlsx")
+    merging("D:/CoronaVirus/data/special_area_excels/", "港澳台数据总表.xlsx")
+    merging("D:/CoronaVirus/data/province_excels/", "各省份新增确诊数据总表.xlsx")
+    merging("D:/CoronaVirus/data/province_excels/", "各省份新增无症状感染者数据总表.xlsx")
     merging("D:/CoronaVirus/data/main_land_excels/", "中国大陆本土数据总表.xlsx")
