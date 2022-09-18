9d80967291fb38fa6c2ec46cdf32b7afa10e4daf
GodotHXB <371502435@qq.com>
2022-09-18 14:11:54 +0800

    province data visualization

--- 032002613/data_visualization.py
+++ 032002613/data_visualization.py
@@ -5,6 +5,9 @@ from matplotlib import font_manager
 plt.rcParams['font.sans-serif'] = ['SimHei']
 my_font = font_manager.FontProperties(fname="C:/WINDOWS/Fonts/STSONG.TTF")
 
+provinces = ['河北', '山西', '辽宁', '吉林', '黑龙江', '江苏', '浙江', '安徽', '福建', '江西', '山东', '河南', '湖北', '湖南', '广东', '海南',
+             '四川', '贵州', '云南', '陕西', '甘肃', '青海', '内蒙古', '广西', '西藏', '宁夏', '新疆', '北京', '天津', '上海', '重庆']
+
 
 def make_mainland_newly_infected():
     # 生成大陆本土新增确诊数据图
@@ -30,7 +33,6 @@ def make_mainland_newly_infected():
 
     plt.title("中国大陆本土新增确诊病例")
     plt.show()
-    print("successful")
 
 
 def make_mainland_newly_infected_n():
@@ -57,9 +59,79 @@ def make_mainland_newly_infected_n():
 
     plt.title("中国大陆本土新增无症状感染者")
     plt.show()
-    print("successful")
+
+
+def make_province_newly_infected(date):
+    # 各省份某日本土新增确诊病例
+    plt.figure(figsize=(24, 10), dpi=300)
+    data = pd.read_excel('各省份新增确诊数据总表.xlsx')
+    # print(data)
+
+    # 各省份某日本土新增确诊病例
+    infected = data[data['date'] == date]
+    if infected.empty == True:
+        print("输入的日期错误！")
+        return
+    print(infected)
+
+    count = infected['count']
+    # print(count)
+
+    plt.bar(provinces, count)
+
+    plt.xlabel("省份", fontproperties=my_font, fontsize=15)
+    plt.ylabel("新增确诊数量", fontproperties=my_font, fontsize=15)
+    for a, b in zip(provinces, count):
+        plt.text(a, b, format(b, ','), ha='center', fontsize=16)
+    plt.title("各省份" + date + "本土新增确诊病例")
+    plt.show()
+
+
+def make_province_newly_infected_n(date):
+    # 各省份某日本土新增无症状感染者
+    plt.figure(figsize=(24, 10), dpi=300)
+    data = pd.read_excel('各省份新增无症状感染者数据总表.xlsx')
+    # print(data)
+
+    # 各省份某日本土新增确诊病例
+    infected_n = data[data['date'] == date]
+    if infected_n.empty == True:
+        print("输入的日期错误！")
+        return
+    # print(infected_n)
+
+    count = infected_n['count']
+    # print(count)
+
+    plt.bar(provinces, count)
+
+    plt.xlabel("省份", fontproperties=my_font, fontsize=15)
+    plt.ylabel("新增无症状感染者数量", fontproperties=my_font, fontsize=15)
+    for a, b in zip(provinces, count):
+        plt.text(a, b, format(b, ','), ha='center', fontsize=16)
+    plt.title("各省份" + date + "本土新增无症状感染者")
+    plt.show()
 
 
 if __name__ == '__main__':
-    # make_mainland_newly_infected()
-    make_mainland_newly_infected_n()
+    mode = int(input("""请输入想要生成的数据类型：
+1.中国大陆本土新增确诊病例
+2.中国大陆本土新增无症状感染者
+3.各省份某日本土新增确诊病例
+4.各省份某日本土新增无症状感染者
+5.港澳台累计确诊人数
+（输入1-5的某个数字，输入0退出）：
+"""))
+
+    if mode == 1:
+        make_mainland_newly_infected()
+    if mode == 2:
+        make_mainland_newly_infected_n()
+    if mode == 3:
+        date = input("输入某个日期（例：2021年7月9日）：")
+        make_province_newly_infected(date)
+    if mode == 4:
+        date = input("输入某个日期（例：2021年7月9日）：")
+        make_province_newly_infected_n(date)
+    if (mode == 0):
+        pass
